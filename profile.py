c0ac2ac7b662ffc320874c052faa0cc94e39e0ec
angelolacuestabenilde <angelogiuseppe.lacuesta@benilde.edu.ph>
2026-05-22 23:28:19 +0800

    Initial commit: Add basic profile variables

---
+++ profile.py
@@ -0,0 +1,23 @@
+# Profile Generator - Version 1.0
+
+name = "Angelo"  # String tracking alphabetic values
+
+age = 19  # Integer tracking discrete values
+
+is_student = True  # Boolean tracking state/conditions
+
+print("Name:", name)
+
+print("Age:", age)
+
+print("Student Status:", is_student)
+
+# Appending new collection structures and calculations
+
+skills = ["Python", "Git", "HTML"]  # List collection type
+
+years_to_graduation = 4 - 1.5  # Float calculation evaluation
+
+print("Skills:", skills)
+
+print("Years left to graduate:", years_to_graduation)
